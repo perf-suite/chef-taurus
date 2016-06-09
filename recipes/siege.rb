cf21e2a72096ba5d5f177965be9d898988c71aa8
perf-tools <perf-tools@outlook.com>
2016-06-09 15:33:03 -0400

    feature add engine siege

---
+++ recipes/siege.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: taurus
+# Recipe:: siege 
+#
+
+package_list = value_for_platform('ubuntu' => { 'default' => node['taurus']['siege']['package']['list_ubuntu'] },
+                                  'default' => node['taurus']['siege']['package']['list'])
+
+package_list.each do |package_name|
+  package package_name do
+    action :install
+  end
+end
